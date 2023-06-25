b3ccbc99dd611515f03dc38aeea3deb1080ac2d1
Aaron C Wacker <awacke1@users.noreply.huggingface.co>
2023-06-25 13:58:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,8 @@ def chat_with_model(prompt, document_section, model_choice='gpt-3.5-turbo'):
     # streaming response
     result_textarea = st.empty()
     results=[]
-    for responses in openai.ChatCompletion.create(model=model, messages=conversation, stream=True):
+    for responses in openai.Completion.create(model=model, messages=conversation, stream=True):
+#    for responses in openai.ChatCompletion.create(model=model, messages=conversation, stream=True):
         #results.append(str(responses.choices[0]))
         results.append(responses.choices[0].text)
         #st.markdown(f'*{results}*')
